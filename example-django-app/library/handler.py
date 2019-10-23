961b5b7eee3798f2195c2a7f17d0a9700a37a309
Kyle Hornberg <kyle.hornberg@gmail.com>
2019-10-23 14:44:38 -0500

    Add more wsgi fields

--- example-django-app/library/handler.py
+++ example-django-app/library/handler.py
@@ -1,7 +1,22 @@
-import json
 import os
 from importlib import import_module
 from werkzeug.wrappers import Response
+
+
+def get_wsgi_environ(event):
+    return {
+        "REQUEST_METHOD": "get",
+        "PATH": "/books/",
+        "wsgi.input": None,
+        "SERVER_NAME": "localhost",
+        "SERVER_PORT": "80",
+        'SERVER_PROTOCOL': str('HTTP/1.1'),
+        'wsgi.version': (1, 0),
+        "wsgi.run_once": False,
+        "wsgi.multiprocess": False,
+        "wsgi.multithread": False,
+        'wsgi.url_scheme': 'http',
+    }
 
 
 def get_wsgi_response(app, environ):
@@ -10,34 +25,28 @@ def get_wsgi_response(app, environ):
 
 # https://github.com/django/django/blob/4b4e68a7a6847e8b449923bb882bed01f0d7b2a8/django/utils/module_loading.py
 def import_string(dotted_path):
-    """
-    Import a dotted module path and return the attribute/class designated by the
-    last name in the path. Raise ImportError if the import failed.
-    """
     try:
-        module_path, class_name = dotted_path.rsplit('.', 1)
+        module_path, class_name = dotted_path.rsplit(".", 1)
     except ValueError as err:
         raise ImportError("%s doesn't look like a module path" % dotted_path) from err
-
     module = import_module(module_path)
-
     try:
         return getattr(module, class_name)
     except AttributeError as err:
-        raise ImportError('Module "%s" does not define a "%s" attribute/class' % (
-            module_path, class_name)
-        ) from err
+        raise ImportError('Module "%s" does not define a "%s" attribute/class' % (module_path, class_name)) from err
+
+
+def get_app():
+    app_path = os.environ.get("WSGI_APPLICATION")
+    return import_string(app_path)
 
 
 def handler(event, context):
-    app_path = os.environ.get('WSGI_APPLICATION')
-    app = import_string(app_path)
-    environ = {
-        "REQUEST_METHOD": "get",
-        "PATH": "/books/",
-        "wsgi.input": None,
-        "SERVER_NAME": "localhost",
-        "SERVER_PORT": "80",
-    }
+    app = get_app()
+    environ = get_wsgi_environ(event)
     response = get_wsgi_response(app, environ)
-    return {"status_code": response.status_code, "body": json.dumps(response.get_data(as_text=True))}
+    return {
+        "status_code": response.status_code,
+        "body": response.get_data(as_text=True),
+        "headers": response.headers,
+    }
--- example-django-app/library/tests/handler_test.py
+++ example-django-app/library/tests/handler_test.py
@@ -3,22 +3,43 @@ import json
 from library.handler import handler
 
 
-def assert_response(response, expected_body='', expected_status_code=200):
-    assert response['status_code'] == expected_status_code
-    assert json.loads(response['body']) == expected_body
+def assert_response(response, expected_body="", expected_status_code=200):
+    headers = dict(response["headers"])
+    assert response["status_code"] == expected_status_code
+    if headers.get("Content-Type") in ["application/json"]:
+        assert json.loads(response["body"]) == expected_body
+    else:
+        assert response["body"] == expected_body
 
 
 def test_handler():
-    os.environ['WSGI_APPLICATION'] = 'wsgiref.simple_server.demo_app'
+    os.environ["WSGI_APPLICATION"] = "wsgiref.simple_server.demo_app"
     event = {}
     context = {}
-    expected = "Hello world!\n\nPATH = '/books/'\nREQUEST_METHOD = 'get'\nSERVER_NAME = 'localhost'\nSERVER_PORT = '80'\nwsgi.input = None\n"
+    expected = """Hello world!
+
+PATH = '/books/'
+REQUEST_METHOD = 'get'
+SERVER_NAME = 'localhost'
+SERVER_PORT = '80'
+SERVER_PROTOCOL = 'HTTP/1.1'
+wsgi.input = None
+wsgi.multiprocess = False
+wsgi.multithread = False
+wsgi.run_once = False
+wsgi.url_scheme = 'http'
+wsgi.version = (1, 0)
+"""
     assert_response(handler(event, context), expected)
 
 
 def test_handler_with_django():
-    os.environ['WSGI_APPLICATION'] = 'project.wsgi.application'
+    os.environ["WSGI_APPLICATION"] = "project.wsgi.application"
     event = {}
     context = {}
-    expected = '{"authors":"None://localhost/authors/","books":"None://localhost/books/","paper-sources":"None://localhost/paper-sources/"}'
+    expected = {
+        "authors": "http://localhost/authors/",
+        "books": "http://localhost/books/",
+        "paper-sources": "http://localhost/paper-sources/",
+    }
     assert_response(handler(event, context), expected)
