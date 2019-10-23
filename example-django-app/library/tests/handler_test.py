440839dcba9aba1ab9c247374c459328e8feb63b
Kyle Hornberg <kyle.hornberg@gmail.com>
2019-10-23 13:53:02 -0500

    Fix tests

--- example-django-app/library/tests/handler_test.py
+++ example-django-app/library/tests/handler_test.py
@@ -9,7 +9,7 @@ def assert_response(response, expected_body='', expected_status_code=200):
 
 
 def test_handler():
-    os.environ.setdefault('WSGI_APPLICATION', 'wsgiref.simple_server.demo_app')
+    os.environ['WSGI_APPLICATION'] = 'wsgiref.simple_server.demo_app'
     event = {}
     context = {}
     expected = "Hello world!\n\nPATH = '/books/'\nREQUEST_METHOD = 'get'\nSERVER_NAME = 'localhost'\nSERVER_PORT = '80'\nwsgi.input = None\n"
@@ -17,7 +17,7 @@ def test_handler():
 
 
 def test_handler_with_django():
-    os.environ.setdefault('WSGI_APPLICATION', 'project.wsgi.application')
+    os.environ['WSGI_APPLICATION'] = 'project.wsgi.application'
     event = {}
     context = {}
     expected = '{"authors":"None://localhost/authors/","books":"None://localhost/books/","paper-sources":"None://localhost/paper-sources/"}'
